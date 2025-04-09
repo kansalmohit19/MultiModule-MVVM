1887107385261b6379afecadca1f54903633acc1
Mohit Kansal <kansalmohit19@gmail.com>
2025-04-09 13:33:02 +0530

    Plugin code re-formatting

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("kotlin-kapt")
-    id("com.google.dagger.hilt.android")
+    kotlin("kapt")
+    alias(libs.plugins.dagger)
 }
 
 android {
