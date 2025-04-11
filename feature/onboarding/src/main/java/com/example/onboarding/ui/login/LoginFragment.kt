247e10b5685a47841c10f550a39ed077649cf943
Mohit Kansal <kansalmohit19@gmail.com>
2025-04-11 12:36:06 +0530

    Handling of navigation of Onboarding inner module

--- feature/onboarding/src/main/java/com/example/onboarding/ui/login/LoginFragment.kt
+++ feature/onboarding/src/main/java/com/example/onboarding/ui/login/LoginFragment.kt
@@ -3,14 +3,16 @@ package com.example.onboarding.ui.login
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.example.feature.login.R
 import com.example.feature.login.databinding.FragmentLoginBinding
+import com.example.navigator.ModuleType
 import com.example.onboarding.ui.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class LoginFragment : BaseFragment() {
-    private val viewModel: LoginViewModel by viewModels()
+    // private val viewModel: LoginViewModel by viewModels()
 
     private lateinit var binding: FragmentLoginBinding
 
@@ -21,7 +23,12 @@ class LoginFragment : BaseFragment() {
 
     override fun setupUI() {
         binding.btnLogin.setOnClickListener {
-            viewModel.loginUser("mohit", "12345678")
+            // viewModel.loginUser("mohit", "12345678")
+            navigator.navigateToFeatureModule(ModuleType.HomeModule)
+        }
+
+        binding.btnRegister.setOnClickListener {
+            findNavController().navigate(R.id.registerFragment)
             // navigator.navigateToFeatureModule(ModuleType.HomeModule)
         }
     }
